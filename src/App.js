2197f0ad84beff2272adb002ff510764ae8bcae4
VictorChagasS <victorsantoschagas@hotmail.com>
2024-04-10 18:56:14 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Chess from "./components/chess";
+import Header from "./components/header";
+import Sudoku from "./components/sudoku";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element={<Sudoku />} path="" />
+          <Route element={<Chess />} path="chess" />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
